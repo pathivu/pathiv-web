934942f5c6ba8252b8e76d947b2547c6b960db90
Rishi Raj <rishiraj824@gmail.com>
2020-01-14 16:08:31 -0500

    added initial design interface

---
+++ src/components/sidebar/sidebar.js
@@ -0,0 +1,14 @@
+import React, { Component } from './node_modules/react';
+import styles from './sidebar'
+
+export class Sidebar extends Component {
+    render() {
+        return (
+            <div className={styles.sidebar}>
+                
+            </div>
+        )
+    }
+}
+
+export default Sidebar
